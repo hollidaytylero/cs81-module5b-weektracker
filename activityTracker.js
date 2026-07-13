43fdf4439842b84fa9a1a27e62d917e4313b51e4
hollidaytylero <holliday_tyler_oli01@student.smc.edu>
2026-07-13 11:36:45 -0700

    added predictions and 2 analysis functions

--- activityTracker.js
+++ activityTracker.js
@@ -57,3 +57,29 @@ const myWeek = [
     timeOfDay: "evening"
   }
 ];
+
+Predictions
+Which activity will have the highest enjoyment?
+My athletic and workout activities will have the highest enjoyment. Moving and sports are a big part of my life.
+  
+What category will dominate your week?
+Physical activities will dominate my week because it is such a big part of my life and also because it is summer so I have a lot more time for physical training and activities.
+
+What patterns might exist around time of day?
+Most of my physical activities will be in the morning and my other social and family activities will be in the afternoon or evening. This is due to my schedule and my parents/friends schedule.
+
+Here is 1 function:   
+// Find activities with low hours but high enjoyment
+function lowHoursHighEnjoyment() {
+  return myWeek.filter(
+    activity => activity.hoursSpent < 2 && activity.enjoyment > 8
+  );
+
+Here is 2 function:
+// Give me the names of activities with an enjoyment score of 9 or higher
+function favoriteActivities() {
+  return myWeek
+    .filter(activity => activity.enjoyment >= 9)
+    .map(activity => activity.activity);
+}
+}
